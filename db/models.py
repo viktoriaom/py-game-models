26fe22a166934cd06f7443466b9cf7a0f3d69f7a
Vik <viktoria.om@gmail.com>
2025-04-13 12:50:06 +0300

    'Solution'

--- db/models.py
+++ db/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+from django.db.models import PROTECT
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        data = json.load(file)
+
+    for player in data:
+        this_player_race = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            description=player["race"]["description"])
+
+        Skill.objects.get_or_create(
+            name=player["race"]["skills"]["name"],
+            bonus=player["race"]["skills"]["bonus"],
+            race=this_player_race)
+
+        Guild.objects.get_or_create(
+            name=player["guild"]["name"],
+            description=player["guild"]["description"])
+
+        Player.objects.create(
+            nickname=player[0],
+            email=player["email"],
+            bio=player["bio"],
+            race=this_player_race,
+            guild=Guild.objects.update_or_create(
+                name=player["guild"]["name"],
+                description=player["guild"]["description"]))
 
 
 if __name__ == "__main__":
